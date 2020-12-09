12b262bd2d73ba4581136679c85244f829100ac9
Max Chen <maxchen.sihhan@gmail.com>
2020-12-09 17:29:04 +0800

    before update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAUYYfg5_fTLisiq3WqP4EC2wsH_7P_zc4',
+    authDomain: 'secret-santa-gen.firebaseapp.com',
+    projectId: 'secret-santa-gen',
+    storageBucket: 'secret-santa-gen.appspot.com',
+    messagingSenderId: '312399944019',
+    appId: '1:312399944019:web:4ebd4fd2c09526036769a6',
+    measurementId: 'G-JK37D6RDY5',
+  },
 };
